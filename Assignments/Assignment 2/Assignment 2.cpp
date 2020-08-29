0710f30c65bdd82ea1778e2dd1b7d3958b4c5503
Luke Lynch <tech.luk3@gmail.com>
2020-08-29 17:23:13 +1000

    Reorganise Solution. Added Assignment 2 project

---
+++ Assignments/Assignment 2/Assignment 2.cpp
@@ -0,0 +1,13 @@
+// Assignment 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS	// using OpenCL 1.2, some functions deprecated in OpenCL 2.0
+#define __CL_ENABLE_EXCEPTIONS	
+
+#include <iostream>
+#include <CL\cl.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+

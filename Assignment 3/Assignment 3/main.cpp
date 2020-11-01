7f5550ee4f8a173d6111ebc6a3c04d90f72494b4
Luke Lynch <Tech.Luk3@gmail.com>
2020-11-02 01:39:21 +1100

    added test to read single pixel rgba value

--- Assignment 3/Assignment 3/main.cpp
+++ Assignment 3/Assignment 3/main.cpp
@@ -2,7 +2,27 @@
 #include "common.h"
 #include "bitmap.h"
 
-int main()
+int main(void)
 {
+
+	// declare data and memory objects
+	unsigned char* inputImage;
+	unsigned char* outputImage;
+	int imgWidth, imgHeight, imageSize;
+
+
+	// read input image
+	inputImage = read_BMP_RGB_to_RGBA("peppers.bmp", &imgWidth, &imgHeight);
+
+	// allocate memory for output image
+	imageSize = imgWidth * imgHeight * 4;
+
+	for (int i = 0; i < 4; i++) {
+		std::cout << (int)inputImage[i] << " ";
+	}
+	std::cout << std::endl;
+
+	quit_program("Finished.");
+
 	return 0;
 }
